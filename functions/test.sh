8eccbd76ec40c2b5dac980c735026f9ea3a444b4
alexandre-Maury <alexandre_maury@outlook.com>
2025-01-01 22:10:01 +0100

    maj

--- functions/test.sh
+++ functions/test.sh
@@ -35,7 +35,8 @@ test_disk() {
 
     FREE_START=$(echo "$SELECTED_SPACE" | sed -n 's/.*Start=\([0-9.]*\)MiB.*/\1/p')
     FREE_END=$(echo "$SELECTED_SPACE" | sed -n 's/.*End=\([0-9.]*\)MiB.*/\1/p')
-    FREE_TOTAL=$(echo "$SELECTED_SPACE" | sed -n 's/.*Disponible = \([0-9.]*\)MiB.*/\1/p')
+
+    FREE_TOTAL=$(FREE_END)
 
     echo "Sur l'espace sélectionné :"
     echo "FREE_START ==> $FREE_START"
