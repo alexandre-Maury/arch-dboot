00625d61338f0690cd848e1821a78f1a19292055
alexandre-Maury <alexandre_maury@outlook.com>
2025-01-03 12:25:47 +0100

    maj

--- functions/test.sh
+++ functions/test.sh
@@ -15,7 +15,7 @@ test() {
     for part in "${partitions[@]}"; do
 
         local label=$(lsblk "/dev/$part" -n -o LABEL)
-        local fs_type=$(lsblk -n -o "/dev/$part" -n -o FSTYPE)
+        local fs_type=$(lsblk -n -o "/dev/$part" FSTYPE)
 
         echo "type de la partition pour $part : $fs_type"
 
