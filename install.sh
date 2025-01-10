a66c49ce4db45fed84ae6cd7e20a35f0952438b5
alexandre-Maury <alexandre_maury@outlook.com>
2025-01-10 19:24:48 +0100

    maj

--- install.sh
+++ install.sh
@@ -81,11 +81,9 @@ while true; do
     echo "----------------------------------------"
 
     # partitions=$(lsblk -n -o NAME "/dev/$disk" | grep -v "^$disk$" | sed -n "s/^[[:graph:]]*${disk}\([0-9]*\)$/${disk}\1/p")
-    disk_prefix=$(get_disk_prefix "$disk")
-
-    echo "le resultat de disk_prefix : $disk$disk_prefix"
-    echo
     partitions=$(lsblk -n -o NAME "/dev/$disk" | grep -v "^$disk$" | sed -n "s/^[[:graph:]]*${disk}${disk_prefix}\([0-9]*\)$/${disk}${disk_prefix}\1/p")
+
+    echo "mes partitions : $partitions"
 
 
     # Vérifie si des partitions existent
